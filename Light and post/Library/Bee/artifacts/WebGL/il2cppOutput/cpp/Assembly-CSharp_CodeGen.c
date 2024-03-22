cd75cf3a5a29687d1a0cc97f6548e8b13cd1d8e6
linuxgaming25 <127547557+linuxgaming25@users.noreply.github.com>
2024-03-22 16:21:11 -0700

    Build fix

---
+++ Light and post/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void CameraRotator_Update_m452C324B95843B3E07383761CC193F4A5DB64FCD (void);
+extern void CameraRotator__ctor_m9B9A4AC500C47195F6356BECA9A41E5EEB4FC035 (void);
+extern void RotatingObject_Update_m4FBE85C4B64F777BCB1405770CEC7ED2B8889D10 (void);
+extern void RotatingObject__ctor_m1B9DB33AC1B1FF6E9B766CEBA0ED072B07488C37 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CameraRotator_Update_m452C324B95843B3E07383761CC193F4A5DB64FCD,
+	CameraRotator__ctor_m9B9A4AC500C47195F6356BECA9A41E5EEB4FC035,
+	RotatingObject_Update_m4FBE85C4B64F777BCB1405770CEC7ED2B8889D10,
+	RotatingObject__ctor_m1B9DB33AC1B1FF6E9B766CEBA0ED072B07488C37,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	1371,
+	1371,
+	1371,
+	1371,
+	2512,
+	1371,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
